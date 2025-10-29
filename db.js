eab747e3755b3fd45a2bfccbbb48fa3742e075f7
Guilherme Bastos <guibastos3003@gmail.com>
2025-10-29 15:01:34 -0300

    Adição do dialeto

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
+const mysql2 = require('mysql2');
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
+    dialectModule: mysql2,
     define: {
         charset: 'utf8',
         collate: 'utf8_general_ci',
